35fdcfbf4401e4b31707f66c20b8c1b65491a6c9
@jewtechx <jklarbi_danquah@st.ug.edu.gh>
2023-09-08 22:41:17 +0000

    back to top

--- src/components/backToTop.tsx
+++ src/components/backToTop.tsx
@@ -6,9 +6,9 @@ import ScrollToTop from 'react-scroll-up'
 export default function BackToTop() {
 
   return (
-    <div className={`fixed w-full text-right z-20 cursor-pointer`}>
+    <div className={` fixed w-full text-right z-20 cursor-pointer`}>
       <div
-        className=" fixed right-10 bottom-10 z-40 shadow-md rounded-full w-14 h-14 flex items-center justify-center bg-slate-50 border-[0.2px] border-blue-100 text-center text-lg font-[600]"
+        className=" animate-bounce fixed right-10 bottom-10 z-40 shadow-md rounded-full w-14 h-14 flex items-center justify-center bg-slate-50 border-[0.2px] border-blue-100 text-center text-lg font-[600]"
         onClick={() => {
           window.scrollTo(0,0);
           alert(scrollY)
